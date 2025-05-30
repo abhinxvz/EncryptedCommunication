cf391cbc1115d5e5d8ff82a47e892834f79bb79a
abhinxvz <abhinxvz@users.noreply.github.com>
2025-05-30 11:30:00 +0530

    Add comprehensive test suite

---
+++ test/Test_AES256EncryptorHandler.cpp
@@ -0,0 +1,187 @@
+#include <random>
+#include <stdexcept>
+
+#include "AES256EncryptorHandler.hpp"
+#include "Utils.hpp"
+
+bool test_saveKeyToFileAndSaveCyphertextThenReadAndDecrypt()
+{
+    AES256EncryptorHandler test;
+
+    const std::string key = test.generateKey();
+
+    const std::string message = "This is a secret message! I hope no one is able to decrypt it";
+
+    writeFile("sKey.txt",key);
+    writeFile("cyphertext.txt",test.encrypt(message,key));
+
+    const std::string rKey = readFile("sKey.txt"); 
+    const std::string cyphertext = readFile("cyphertext.txt");
+
+    deleteFile("sKey.txt");
+    deleteFile("cyphertext.txt");
+
+    return message == test.decrypt(cyphertext,rKey);
+}
+
+bool test_noInitVector()
+{
+    AES256EncryptorHandler test;
+
+    std::string cypherText = test.encrypt("Hello","12345");
+
+    bool result;
+
+    try
+    {
+        test.decrypt("c","1234");
+        result = false;
+    }
+    catch(const std::length_error& e)
+    {
+        result = true;
+    }
+
+    return result;
+}
+
+bool test_differentKey()
+{
+    AES256EncryptorHandler test;
+
+    std::string cypherText = test.encrypt("Hello","12345");
+    std::string plainText = test.decrypt(cypherText,"1234");
+
+    return plainText != "Hello";
+}
+
+bool test_emptyString()
+{
+    AES256EncryptorHandler test;
+
+    const std::string key = test.generateKey();
+
+    const std::string message = "";
+    bool result;
+    try
+    {
+        test.encrypt(message,key);
+        result = false;
+    }
+    catch(const std::length_error& e)
+    {
+        result = true;
+    }
+
+    return result;
+}
+
+bool test_edgeCases()
+{
+    if(!test_emptyString())
+        return false;
+
+    if(!test_differentKey())
+        return false;
+
+    if(!test_noInitVector())
+        return false;
+
+    return true;
+}
+
+bool test_stressTest_randomKeyRandomLengthMessage()
+{
+    /* Maximum 1 MB messages */
+    const int maximumNumberOfBytesOfTheMessage = 1000000;
+    const int numberOfIterations = 1000;
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(1,maximumNumberOfBytesOfTheMessage);
+
+    AES256EncryptorHandler test;
+
+    for(int i = 0 ; i < numberOfIterations; i++)
+    {
+        const std::string key = test.generateKey();
+
+        const std::string message = generateRandomString(distribution(generator));
+
+        const std::string cyphertext = test.encrypt(message,key);
+    
+        if(message != test.decrypt(cyphertext,key))
+            return false;
+    }
+
+    return true;
+}
+
+bool test_stressTest_sameKeyRandomLengthMessage()
+{
+    /* Maximum 1 MB messages */
+    const int maximumNumberOfBytesOfTheMessage = 1000000;
+    const int numberOfIterations = 1000;
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(1,maximumNumberOfBytesOfTheMessage);
+
+    AES256EncryptorHandler test;
+
+    const std::string key = test.generateKey();
+
+
+    for(int i = 0 ; i < numberOfIterations; i++)
+    {
+        const std::string message = generateRandomString(distribution(generator));
+
+        const std::string cyphertext = test.encrypt(message,key);
+    
+        if(message != test.decrypt(cyphertext,key))
+            return false;
+    }
+
+    return true;
+}
+
+bool test_randomMessages_2048Bit()
+{
+    AES256EncryptorHandler test;
+    const std::string key = test.generateKey();
+    const std::string message = generateRandomString(2048);
+    const std::string cyphertext = test.encrypt(message,key);
+    return message == test.decrypt(cyphertext,key);
+}
+
+bool test_helloWorld()
+{
+    AES256EncryptorHandler test;
+    const std::string key = test.generateKey();
+    const std::string message = "Hello world!";
+    const std::string cyphertext = test.encrypt(message,key);
+    return message == test.decrypt(cyphertext,key);
+}
+
+int main (int argc, char* argv[])
+{
+    if(!test_helloWorld())
+        return -1;
+
+    if(!test_randomMessages_2048Bit())
+        return -1;
+
+    if(!test_stressTest_sameKeyRandomLengthMessage())
+        return -1;
+
+    if(!test_stressTest_randomKeyRandomLengthMessage())
+        return -1;
+
+    if(!test_edgeCases())
+        return -1;
+
+    if(!test_saveKeyToFileAndSaveCyphertextThenReadAndDecrypt())
+        return -1;
+        
+    return 0;
+}
---
+++ test/Test_RSAEncryptorHandler.cpp
@@ -0,0 +1,107 @@
+#include <random>
+
+#include "RSAEncryptorHandler.hpp"
+#include "SHAHashHandler.hpp"
+#include "Utils.hpp"
+
+bool test_signatureStressTest()
+{
+    RSAEncryptorHandler test;
+    SHA256HashHandler hasher;
+
+    uint numberOfIterations = 500;
+
+    for(uint i=0 ; i< numberOfIterations ; i++)
+    {
+        RsaKeyPair res =  test.generateKeyPair(2048);
+    
+        const std::string message = generateRandomString(1000);
+     
+        const std::string messageDigest = hasher.hash(message);
+    
+        const std::string signedDigest = test.signMessageDigestSha256(messageDigest,res.privateKey);
+        if (!test.verifyMessageDigestSha256(messageDigest,signedDigest,res.publicKey))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool test_stressTest_randomKeyRandomLengthMessage()
+{
+    const int maximumNumberOfBytesOfTheMessage = 128;
+    const int numberOfIterations = 500;
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(1,maximumNumberOfBytesOfTheMessage);
+
+    RSAEncryptorHandler test;
+
+    for(int i = 0 ; i < numberOfIterations; i++)
+    {
+        const RsaKeyPair key = test.generateKeyPair(2048);
+
+        const std::string message = generateRandomString(distribution(generator));
+
+        const std::string cyphertext = test.encrypt(message,key.publicKey);
+    
+        const std::string decryptedMessage = test.decrypt(cyphertext,key.privateKey);
+
+        if(message != decryptedMessage)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool test_signature()
+{
+    RSAEncryptorHandler test;
+    RsaKeyPair res =  test.generateKeyPair(2048);
+    const std::string message = "Hello world!";
+
+    SHA256HashHandler hasher; 
+    const std::string messageDigest = hasher.hash(message);
+    const std::string signedDigest = test.signMessageDigestSha256(messageDigest,res.privateKey);
+    const bool verifyResult = test.verifyMessageDigestSha256(messageDigest,signedDigest,res.publicKey);
+
+    return verifyResult;
+}
+
+bool test_helloWorld()
+{
+    RSAEncryptorHandler test;
+        
+    RsaKeyPair res =  test.generateKeyPair(2048);
+
+    const std::string message = "Hello world!";
+
+    const std::string cyphertext = test.encrypt(message,res.publicKey);
+    
+    /* RSA at 2048 bits supports messages of maximum 256 bytes */
+    const std::string decryptedMessage = test.decrypt(cyphertext,res.privateKey).substr(0,message.size());
+
+    return message == decryptedMessage;
+}
+
+int main (int argc, char* argv[])
+{
+    if(!test_helloWorld())
+        return -1;
+
+    if(!test_signature())
+        return -1;
+
+    if(!test_stressTest_randomKeyRandomLengthMessage())
+        return -1;
+
+    if(!test_signatureStressTest())
+        return -1;
+
+    return 0;
+}
